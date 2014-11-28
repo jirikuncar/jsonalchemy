06c8711914ac48ade7bf2437b30cbf94c0334378
Mateusz Susik <mateusz.susik@cern.ch>
2014-11-28 10:27:15 +0100

    authors: base record
    * Defines fields and models for author base record.
    
    * Adds new function for jsonalchemy models definitions.
    
    * Includes tests for author base record.
    
    Signed-off-by: Mateusz Susik <mateusz.susik@cern.ch>
    Reviewed-by: Jiri Kuncar <jiri.kuncar@cern.ch>

--- invenio/modules/jsonalchemy/jsonext/functions/int_util_split.py
+++ invenio/modules/jsonalchemy/jsonext/functions/int_util_split.py
@@ -17,24 +17,21 @@
 ## along with Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-"""Function for tokenizing strings in models' files."""
+"""Function for splitting string into integers in models' files."""
+
+from .util_split import util_split
 
 
-def util_split(string, separator, index):
-    """
-    Helper function to split safely a string and get the n-th element.
+def int_util_split(string, separator, index):
+    """Helper function to split safely a string and get the n-th number.
 
     :param string: String to be split
     :param separator:
     :param index: n-th part of the split string to return
 
-    :return: The n-th part of the string or empty string in case of error
+    :return: The n-th number of the string or None in case of error
     """
-    print string
-    print separator
-    print index
-    string_splitted = string.split(separator)
     try:
-        return string_splitted[index].strip()
-    except:
-        return ""
+        return int(util_split(string, separator, index))
+    except ValueError:
+        return None
